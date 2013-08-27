0e40beede94ddf123f4df67690f8af16b5a19551
Pedro Markun <pedro@markun.com.br>
2013-08-27 12:16:15 -0300

    fixing bug na renderizaçao;processando o \emph fix #8

--- script.py
+++ script.py
@@ -12,16 +12,18 @@ def process_fonetica(fonema):
 def parse_verbetes(filename='data/VERBETES.tex'):
 	verbetes_raw = codecs.open(filename, 'r', encoding='utf-8').read()
 
+	emph = re.compile(r'\\emph{(.*?)}') 
 	italic = re.compile(r'\\textit{(.*?)}')
 	bold = re.compile(r'\\textbf{(.*?)}')
 	turna = re.compile(r'{\\textturna}')
 	verb = re.compile(r"\\verb{(.*?)}{(.*?)}{(.*?)}{(.*?)}{(.*?)}{(.*?)}{(.*?)}{(.*?)}{(.*?)}")
-	tilda = re.compile(r"\\\~\{{0,1}(.)\}{0,1}")
+	tilda = re.compile(r"\\\~[{]{0,1}(.)[}]{0,1}")
 	verbetes_raw = verbetes_raw.replace("\n", " ")
-	verbetes_raw = re.sub(italic, "*\\1*", verbetes_raw)
-	verbetes_raw = re.sub(bold, "*\\1*", verbetes_raw)
 	verbetes_raw = re.sub(turna, u"\u0250", verbetes_raw)
 	verbetes_raw = re.sub(tilda, r'\1'+u"\u0303", verbetes_raw)
+	verbetes_raw = re.sub(italic, "*\\1*", verbetes_raw)
+	verbetes_raw = re.sub(emph, "*\\1*", verbetes_raw)
+	verbetes_raw = re.sub(bold, "*\\1*", verbetes_raw)
 	verbetes = []
 	for v in re.findall(verb, verbetes_raw):
 		verbete = {}
